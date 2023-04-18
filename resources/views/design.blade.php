216cc51c5c43da413802c73cc27cfde92bc6e15a
nicklasos <nicklasos@gmail.com>
2023-04-18 19:16:48 +0300

    Main page design

--- resources/views/design.blade.php
+++ resources/views/design.blade.php
@@ -69,14 +69,56 @@
         </div>
 
     </header>
+
+
     <main class="main">
         <div class="main__container">
             <div class="main__text">
                 <h1>TQuiz</h1>
                 <p>Multiplayer tournament trivia games</p>
             </div>
+
+            <div class="tournaments">
+
+                <div class="tournament">
+
+                    <div class="tournament__image">
+                        <img src="/img/tournaments/fortnite.jpg" alt="fortnite">
+                    </div>
+
+                    <div class="tournament__body">
+
+                        <div class="tournament__title">
+                            Fortnite
+                        </div>
+
+                        <div class="tournament__description">
+                            Know everything about Fortnite?
+                        </div>
+
+                        <div class="tournament__footer">
+                            <div class="tournament__params">
+                                <p>5 Players</p>
+                                <p>6 Questions</p>
+                            </div>
+                            <div class="tournament__join">
+                                <button class="join-button">Join</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <br><br>
+
         </div>
+
     </main>
+
+
     <footer class="footer">
         <div class="footer__container">
 
