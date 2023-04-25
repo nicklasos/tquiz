fe165df6ee7c0b54585c2326188fae6a4b3ddb57
nicklasos <nicklasos@gmail.com>
2023-04-25 09:20:41 +0300

    Add leaderboard style

---
+++ resources/views/components/leaderboard.blade.php
@@ -0,0 +1,78 @@
+<div class="leaderboard">
+    <div class="leaderboard__title">
+        In progress...
+    </div>
+    <div class="leaderboard__subtitle">
+        Fortnite
+    </div>
+    <div class="leaderboard__table">
+        <div class="leaderboard__table_header">
+            <div class="col-1">
+                #
+            </div>
+            <div class="col-2">
+                Name
+            </div>
+            <div class="col-3">
+                Score
+            </div>
+        </div>
+        <div class="leaderboard__row position-1">
+            <div class="col-1">
+                <img src="/img/icons/trophy-1.svg" alt="1 place icon">
+                1
+            </div>
+            <div class="col-2">
+                You
+            </div>
+            <div class="col-3">
+                <div class="leaderboard_score">
+                    955
+                </div>
+            </div>
+        </div>
+        <div class="leaderboard__row position-2">
+            <div class="col-1">
+                <img src="/img/icons/trophy-2.svg" alt="2 place icon">
+                2
+            </div>
+            <div class="col-2">
+                Crash Bandicoot
+            </div>
+            <div class="col-3">
+                495
+            </div>
+        </div>
+        <div class="leaderboard__row position-3">
+            <div class="col-1">
+                <img src="/img/icons/trophy-3.svg" alt="3 place icon">
+                3
+            </div>
+            <div class="col-2">
+                Some Dude
+            </div>
+            <div class="col-3">
+                372
+            </div>
+        </div>
+        <div class="leaderboard__row">
+            <div class="col-1">
+                4
+            </div>
+            <div class="col-2">
+                Other Dude
+            </div>
+            <div class="col-3">
+                270
+            </div>
+        </div>
+        <div class="leaderboard__row">
+            <div class="leaderboard__row_waiting">
+                Waiting for participants
+            </div>
+        </div>
+    </div>
+    <div class="leaderboard__buttons">
+
+    </div>
+</div>
