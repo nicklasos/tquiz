ad84fc41141cf1bf706765db5095a5fa7ceb8619
nicklasos <nicklasos@gmail.com>
2023-05-21 14:42:40 +0300

    Add sentry

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,15 @@
 import {router} from "./routes";
+import * as Sentry from "@sentry/browser";
 
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    if (import.meta.env.VITE_APP_ENV === 'production') {
+        Sentry.init({
+            dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
+        });
+    }
+
     router(document.body.getAttribute('data-page'));
 });
+
