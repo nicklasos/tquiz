ba78e46ed7adcfbcade9d05e47eae3fea27e3b5d
nicklasos <nicklasos@gmail.com>
2023-06-08 22:20:58 +0300

    Add filament

--- app/Filament/Resources/QuestionResource.php
+++ app/Filament/Resources/QuestionResource.php
@@ -37,7 +37,7 @@ class QuestionResource extends Resource
                     ->maxLength(65535),
                 Forms\Components\Textarea::make('description')
                     ->maxLength(65535),
-                forms\components\toggle::make('is_active')
+                Forms\components\toggle::make('is_active')
                     ->required(),
             ]);
     }
