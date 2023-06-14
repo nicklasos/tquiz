3571a99fff762c511a4ba12f2461787cf70e9c99
nicklasos <nicklasos@gmail.com>
2023-06-14 13:44:20 +0300

    Add youtube instead of discord

--- config/contact.php
+++ config/contact.php
@@ -3,8 +3,9 @@
 return [
     'email' => 'untrivial.gg@gmail.com',
     'social' => [
-        'tiktok' => 'https://www.tiktok.com/@untrivialgg',
+        'tiktok' => 'https://www.tiktok.com/@untrivial.gg',
         'twitter' => 'https://twitter.com/untrivialgg',
         'discord' => 'https://discord.gg/Z7JzHYEV',
+        'youtube' => 'https://www.youtube.com/@untrivialgg',
     ],
 ];
