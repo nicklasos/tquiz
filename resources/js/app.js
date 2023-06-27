19aabde384542b2104e3cfcde2035f21464a5170
nicklasos <nicklasos@gmail.com>
2023-06-27 10:02:25 +0300

    Remove sentry

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,9 @@
 import {router} from "./routes";
-import * as Sentry from "@sentry/browser";
 import {menuButtonsLoader} from "./menu_buttons";
 
 
 document.addEventListener("DOMContentLoaded", function () {
     if (import.meta.env.VITE_APP_ENV === 'production') {
-        Sentry.init({
-            dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
-        });
     }
 
     menuButtonsLoader();
